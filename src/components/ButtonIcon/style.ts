0cc6ed44f19b8abfac3488df3ea54eadb4d3d76b
André Grillo <andrell.grillo@gmail.com>
2021-06-26 23:52:29 -0300

    tela de splah e signin

---
+++ src/components/ButtonIcon/style.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 56,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  icon: {
+    width: 24,
+    height: 18
+  },
+
+  iconWrapper: {
+    width: 56,
+    height: 56,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderRightColor: theme.colors.line,
+  },
+
+  title: {
+    flex: 1,
+    color: theme.colors.heading,
+    fontSize: 15,
+    textAlign: 'center'
+  }
+
+})
